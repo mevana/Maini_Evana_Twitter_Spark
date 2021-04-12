39a98fd319a5a95ce6c12066a0c07abe21498f86
Madhavi Evana <44774264+mevana@users.noreply.github.com>
2021-04-11 21:48:20 -0400

    Create table and load data

---
+++ SQL Data/Query_one.sql
@@ -0,0 +1,3 @@
+SELECT * FROM mydb.users;
+Select count(*) from statuses where text like "%SpaceX%";
+SELECT * FROM users where users.screen_name = "SpaceX";
--- SQL Data/Second query-Friends_join.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM friends
-		JOIN users ON friends.friend_id = users.user_id
-		JOIN statuses ON statuses.user_id = users.user_id;
-
-
-
